bdeaf17e913f325813275032d198edf3d68bf27d
LewisAngelo11 <luisangel.sotog11@gmail.com>
2026-02-04 20:55:38 -0700

    fix: se incluyó el StrictMode en la app para seguir buenas practicas en el código, y ligeros cambios en App, sobre las rutas

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
 import { createRoot } from 'react-dom/client'
+import { StrictMode } from 'react'
 import './index.css'
 import App from './App.tsx'
 import { BrowserRouter } from 'react-router'
 
 createRoot(document.getElementById('root')!).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+  <StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </StrictMode>
 )
