9e9d019813b9f9fe69cb6c260b079189e6acffc5
LewisAngelo11 <luisangel.sotog11@gmail.com>
2026-02-07 16:54:20 -0700

    style: Avances en los estilos de los proyectos en el dashboard, y se añadieron mas atributos a los proyectos

--- Front/src/components/Project.tsx
+++ Front/src/components/Project.tsx
@@ -1,25 +1,44 @@
+import { Eye } from "@boxicons/react";
 import "./Project.css"
 
 interface ProyectoProp {
     id: number;
     nombre: string;
     descripcion: string;
+    estatus: string;
+    fechaCreacion: string;
     colaboradores: number;
+    procesos: number;
 }
 
-export default function Project( { id, nombre, descripcion, colaboradores }: ProyectoProp ) {
+export default function Project( { nombre, descripcion, estatus, fechaCreacion, colaboradores, procesos }: ProyectoProp ) {
     return (
         <article className="project-container">
             <header className="project-header">
-                <div>
-                    <p>{id}</p>
-                </div>
                 <p>{nombre}</p>
+                <small>{descripcion}</small>
             </header>
-            <section className="project-body">
-                {descripcion}
-                <p>Colaboradores: {colaboradores}</p>
-            </section>
+            <hr />
+            <div className="project-body">
+                <div className="info-project">
+                    <span>{estatus}</span>
+                    <span>{fechaCreacion}</span>
+                </div>
+                <div className="relations-projects">
+                    <div className="stakeholders-container">
+                        <span>{colaboradores}</span>
+                        <p>Colaboradores</p>
+                    </div>
+                    <div className="process-container">
+                        <span>{procesos}</span>
+                        <p>Procesos</p>
+                    </div>
+                </div>
+                <button className="watch-project">
+                    <Eye />
+                    <p>Ver</p>
+                </button>
+            </div>
         </article>
     );
 }
--- Front/src/components/ProjectsList.tsx
+++ Front/src/components/ProjectsList.tsx
@@ -3,11 +3,10 @@ import "./ProjectList.css"
 
 // Datos de prueba, para la UI
 const proyectos = [
-    {id: 1, nombre: "Proyecto 1", descripcion: "", colaboradores: 2},
-    {id: 2, nombre: "Proyecto 2", descripcion: "", colaboradores: 3},
-    {id: 3, nombre: "Proyecto 3", descripcion: "", colaboradores: 2},
-    {id: 4, nombre: "Proyecto 4", descripcion: "", colaboradores: 3}
-]
+    {id: 1, nombre: "Proyecto 1", descripcion: "Descripción del proyecto 1", estatus: "Planificación", fechaCreacion: "1/2/2026", colaboradores: 2, procesos: 2},
+    {id: 2, nombre: "Proyecto 2", descripcion: "Descripción del proyecto 2", estatus: "En Progreso", fechaCreacion: "23/12/2025", colaboradores: 3, procesos: 2},
+    {id: 3, nombre: "Proyecto 3", descripcion: "Descripción del proyecto 3", estatus: "Planificación", fechaCreacion: "4/2/2026", colaboradores: 2, procesos: 2},
+];
 
 export default function ProjectsList() {
     return (
@@ -18,7 +17,16 @@ export default function ProjectsList() {
             <hr />
             <section className="list">
                 {proyectos.map(p => (
-                    <Project id={p.id} nombre={p.nombre} descripcion={p.descripcion} colaboradores={p.colaboradores}/>
+                    <Project
+                        key={p.id}
+                        id={p.id}
+                        nombre={p.nombre}
+                        descripcion={p.descripcion}
+                        estatus={p.estatus}
+                        fechaCreacion={p.fechaCreacion}
+                        colaboradores={p.colaboradores}
+                        procesos={p.procesos}
+                    />
                 ))}
             </section>
         </section>
