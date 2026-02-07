793f40f9958abec12aefec194b2957cb1c6189a6
LewisAngelo11 <luisangel.sotog11@gmail.com>
2026-02-07 00:03:38 -0700

    feat: Implementado el botón de agregar proyecto en el dashboard, y se agregó la dependencia de boxicons para importar componentes de iconos.

--- Front/src/pages/Dashboard.tsx
+++ Front/src/pages/Dashboard.tsx
@@ -1,6 +1,8 @@
 import "./Dashboard.css";
 import HeaderDashboard from "../components/HeaderDashboard"
 import ProjectsList from "../components/ProjectsList";
+import { Plus } from "@boxicons/react"; // Icono del "+" se importa el componente ya que en <i> no funciona
+import { useNavigate } from "react-router";
 
 export default function Dashboard() {
     return (
@@ -8,7 +10,19 @@ export default function Dashboard() {
             <HeaderDashboard/>
             <section className="panel-body">
                 <ProjectsList/>
+                <CreateProject/>
             </section>
         </main>
+    );
+}
+
+function CreateProject() {
+    const navigate = useNavigate();
+    return (
+        <div className="button-container">
+            <button onClick={() => navigate("/projects")} className="button-new-project">
+                <Plus />
+            </button>
+        </div>
     );
 }
