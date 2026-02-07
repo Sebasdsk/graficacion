e4751ad99d5d555ffeda4cb510854082795a8fc8
LewisAngelo11 <luisangel.sotog11@gmail.com>
2026-02-06 23:33:42 -0700

    feat: Avances en el apartado de proyectos, gestión de proyectos.

--- Front/src/pages/Projects.tsx
+++ Front/src/pages/Projects.tsx
@@ -1,15 +1,13 @@
 import { useState, type SetStateAction } from "react";
+import "./Projects.css"
 
 export default function Projects() {
     const [create, setCreate] = useState<boolean>(false);
 
     return (
-        <main className="proyects-page">
-            <header>
-                <h3>Nuevo Proyecto</h3>
-            </header>
+        <main className="projects-page">
             {!create && <ProjectCreate setCreate={setCreate}/>}
-            {create && <ProjectsConfiguration/>}
+            {create && <ProjectsConfiguration setCreate={setCreate}/>}
         </main>
     );
 }
@@ -21,36 +19,50 @@ interface SetCreateProjectProp {
 
 function ProjectCreate({setCreate}: SetCreateProjectProp) {
     return (
-        <form action="sumbit">
-            <div>
-                <label htmlFor="proyect-name">Nombre del proyecto</label>
-                <input type="text" placeholder="Ingrese el nombre"/>
-            </div>
-            <div>
-            <label htmlFor="proyect-description">Descripción del proyecto</label>
-                <textarea name="proyect-description" id="description">Ingrese la descrpción</textarea>
-            </div>
-            <div>
-                <label htmlFor="proyect-problematic">Problemática del proyecto</label>
-                <textarea name="proyect-problematic" id="problematic">Ingrese la problemática</textarea>
-            </div>
-            <button onClick={() => setCreate(true)} className="button-create">Continuar</button>
-        </form>
+        <section className="project-create">
+            <header>
+                <h3>Nuevo Proyecto</h3>
+            </header>
+            <hr />
+            <form action="sumbit" className="form-create-project">
+                <div className="input-name">
+                    <label htmlFor="proyect-name">Nombre del proyecto</label>
+                    <input type="text" placeholder="Ingrese el nombre"/>
+                </div>
+                <div className="input-description">
+                    <label htmlFor="proyect-description">Descripción del proyecto</label>
+                    <textarea name="proyect-description" id="description" placeholder="Ingrese la descripción"></textarea>
+                </div>
+                <div className="input-problematic">
+                    <label htmlFor="proyect-problematic">Problemática del proyecto</label>
+                    <textarea name="proyect-problematic" id="problematic" placeholder="Ingrese la problemática"></textarea>
+                </div>
+                <button onClick={() => setCreate(true)} className="button-create">Continuar</button>
+            </form>
+        </section>
     );
 }
 
-function ProjectsConfiguration() {
+function ProjectsConfiguration({setCreate}: SetCreateProjectProp) {
     return (
-        <section>
-            <header>
-                <p>Configuración del proyecto</p>
+        <section className="project-configuration">
+            <header className="header-project-configuration">
+                <div className="return-button">
+                    <button onClick={() => setCreate(false)}>Atrás</button>
+                </div>
+                <h3>Configuración del proyecto</h3>
             </header>
-            <section>
-                <div>
+            <hr />
+            <div className="body-project-configuration">
+                <article className="process">
                     <button>Gestionar procesos</button>
                     <small>Gestiona los procesos que tiene tu proyecto</small>
-                </div>
-            </section>
+                </article>
+                <article className="stakeholders">
+                    <button>Gestionar stakeholders</button>
+                    <small>Gestiona a los colaboradores en el proyecto</small>
+                </article>
+            </div>
         </section>
     );
 }
