5871ed00f9fed19e694016656db956aab014f4ed
LewisAngelo11 <luisangel.sotog11@gmail.com>
2026-03-21 23:54:20 -0700

    fear: Implementación de la HomePage de la app.

--- Front/src/App.tsx
+++ Front/src/App.tsx
@@ -2,12 +2,14 @@ import { Route, Routes } from "react-router-dom"
 import Dashboard from "./pages/Dashboard"
 import Login from "./pages/Login"
 import ConfigProjects from "./pages/ConfigProjects"
+import HomePage from "./pages/HomePage"
 
 function App() {
   return (
     <>
       <Routes>
-        <Route path="/" element={<Login/>}/>
+        <Route path="/" element={<HomePage/>}/>
+        <Route path="/login" element={<Login/>}/>
         <Route path='/dashboard' element={<Dashboard/>}/>
         <Route path="/config-projects/:id" element={<ConfigProjects/>}/>
       </Routes>
---
+++ Front/src/pages/HomePage.tsx
@@ -0,0 +1,68 @@
+import { useNavigate } from "react-router";
+import "./HomePage.css"
+import { HomeAlt2 } from "@boxicons/react";
+
+export default function HomePage() {
+    return (
+        <main className="main-home">
+            <HeaderHomePage />
+            <section className="section-home">
+                <h1>Bienvenido a <span className="highlight">FLOWTIC</span> Gestor de Proyectos</h1>
+                <p>Tu plataforma para gestionar proyectos de manera eficiente.</p>
+                <button className="button-start">Comenzar</button>
+            </section>
+            <section className="image-section">
+                <div className="image-container">
+                    <img src="./public/FLOWTIC_Dashboard_Image.png" alt="Descripción de la imagen" />
+                </div>
+            </section>
+            <div className="backdrop">
+                <article className="features">
+                    <div className="text-wrapper">
+                        <h2>Configura tus Proyectos y Recopila Información</h2>
+                        <small>Recopila datos valiosos y prepara la información para la toma de decisiones.</small>
+                    </div>
+                    <div className="image-wrapper">
+                        <img src="./public/FLOWTIC_Config_Project_Image.png" alt="Descripción de la imagen" />
+                    </div>
+                </article>
+                <article className="features">
+                    <div className="text-wrapper">
+                        <h2>Gestiona Procesos y Subprocesos de tu Proyecto</h2>
+                        <small>Describe cómo gestionar los diferentes procesos y subprocesos de tu proyecto de manera eficiente.</small>
+                    </div>
+                    <div className="image-wrapper">
+                        <img src="./public/FLOWTIC_Config_Process_Image.png" alt="Descripción de la imagen" />
+                    </div>
+                </article>
+            </div>
+            <FooterHomePage />
+        </main>
+    );
+}
+
+function HeaderHomePage() {
+    const navigate = useNavigate();
+
+    return (
+        <header className="header-home">
+            <span className="logo">FLOWTIC</span>
+            <nav className="nav-home">
+                <ul>
+                    <li><a href="/"><HomeAlt2 size="sm"/>  Inicio</a></li>
+                    <li><a href="/">Acerca De</a></li>
+                    <li><a href="/">Producto</a></li>
+                </ul>
+            </nav>
+            <button className="button-login" onClick={() => navigate("/login")}>Login</button>
+        </header>
+    );
+}
+
+function FooterHomePage() {
+    return (
+        <footer className="footer-home">
+            <p>&copy; 2026 FLOWTIC. Todos los derechos reservados.</p>
+        </footer>
+    );
+}
