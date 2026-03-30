559ff802134b4658cb5ed62880bde8a5557caa28
LewisAngelo11 <luisangel.sotog11@gmail.com>
2026-03-29 23:43:40 -0700

    style: Cambios en el diseño del módulo del login.

--- Front/src/pages/Login.tsx
+++ Front/src/pages/Login.tsx
@@ -1,11 +1,13 @@
-import { useState, type FormEvent } from "react";
+import { useState } from "react";
 import "./Login.css";
 import { useNavigate } from "react-router";
+import { Eye, EyeSlash } from "@boxicons/react";
 
 export default function Login() {
     return (
         <main className="login-page">
             <FormLogin/>
+            <BannerLogin/>
         </main>
     );
 }
@@ -13,16 +15,20 @@ export default function Login() {
 function FormLogin() {
     const [email, setEmail] = useState<string>("");
     const [passw, setPassw] = useState<string>("");
+    const [toggleShowPassw, setToggleShowPassw] = useState<boolean>(false);
+    const [emailError, setEmailError] = useState<string>("");
+    const [errorMessage, setErrorMessage] = useState<string>("");
     const navigate = useNavigate();
 
     const APIURL = "http://localhost:3000/api/auth/login" // Esta url no es la del back por ahora
 
     // Método para iniciar sesión
-    const signIn = async (e: FormEvent) => {
-        e.preventDefault(); // Esto es para que no se refresque la página al presionar el botón
+    const signIn = async () => {
         if (email.trim() === '' || passw.trim() === '') {
-            alert("Por favor llene todos los datos"); // Esto será cambiado
+            setErrorMessage("Por favor llene todos los datos"); // Esto será cambiado
             return;
+        } else {
+            setErrorMessage(""); // Limpia el mensaje de error antes de hacer la petición
         }
 
         try {
@@ -30,52 +36,81 @@ function FormLogin() {
                 method: "POST",
                 headers: { "Content-Type": "application/json" },
                 body: JSON.stringify({
-                    email: email, // Esto puede cambiar ya que el back esté listo
-                    password: passw // Esto también
+                    email: email,
+                    password: passw
                 })
             });
 
             if (!response.ok) {
-                alert("Error al iniciar sesión");
-                return
+                const message = await response.json();
+                setErrorMessage(message.message);
+                return;
             }
             
             const data = await response.json();
             localStorage.setItem("token", data.token); // Guarda el token en localstorage
+            setErrorMessage("");
             navigate("/dashboard");
         } catch (error) {
             console.error(error);
         }
     }
 
+    // Función que valida si el formato del correo es válido
+    const validateEmail = () => {
+        const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        return emailPattern.test(email);
+    }
+
     return (
-        <form onSubmit={signIn} className="login-form">
+        <form action={signIn} className="login-form">
+            <div className="logo-container">
+                <a href="/" className="back-link"><span className="highlight-text">FLOWTIC</span></a>
+            </div>
             <header className="login-header">
                 <h2>Iniciar Sesión</h2>
+                <small>Ingrese sus credenciales para continuar</small>
             </header>
             <hr className="line-divisor-header" />
 
             <section className="inputs-values">
-                <div className="user-input">
-                    <label htmlFor="mail">Correo</label>
+                <div className="email-input">
+                    <label htmlFor="email">Correo</label>
                     <input
                         type="text"
+                        id="email"
                         placeholder="Ingrese su correo"
                         value={email}
                         onChange={(e) => setEmail(e.target.value)}
+                        onBlur={() => setEmailError(validateEmail() || !email ? "" : "Formato de correo inválido")}
                     />
+                    {emailError && (
+                        <span className="email-error-message">{emailError}</span>
+                    )}
                 </div>
                 <div className="passw-input">
-                    <label htmlFor="paassw">Contraseña</label>
+                    <label htmlFor="password">Contraseña</label>
                     <input
-                        type="password" 
+                        type={toggleShowPassw ? "text" : "password"}
+                        id="password"
                         placeholder="Ingrese su contraseña"
                         value={passw}
                         onChange={(e) => setPassw(e.target.value)}
                     />
+                    <button className="btn-show-passw" type="button" onClick={() => setToggleShowPassw(!toggleShowPassw)}>
+                        {toggleShowPassw ? <EyeSlash className="eye-icon" fill="#6c6c6c"/> : <Eye className="eye-icon" fill="#6c6c6c"/>}
+                    </button>
                     <a href="#">¿Olvidó su contraseña?</a>
                 </div>
-                <button className="login-button">Ingresar</button>
+                <button
+                    className="login-button"
+                    disabled={!!emailError || !email || !passw}
+                >
+                    Iniciar Sesión
+                </button>
+                {errorMessage && (
+                    <p className="error-message">{errorMessage}</p>
+                )}
             </section>
 
             <div className="line-divisor-sign-up">
@@ -89,5 +124,25 @@ function FormLogin() {
                 <a href="#">Cree Cuenta</a>
             </div>
         </form>
+    );
+}
+
+function BannerLogin() {
+    const LogoFlowtic = () => {
+        return (
+            <div className="logo-flowtic">
+                <span>FLOWTIC</span>
+            </div>
+        );
+    }
+
+    return (
+        <div className="banner-login">
+            <LogoFlowtic />
+            <div className="banner-content">
+                <h1>Gestiona y recopila información para tus proyectos</h1>
+                <p>Inicia sesión para continuar con la elaboración de tus proyectos</p>
+            </div>
+        </div>
     );
 }
